d3315a096fff4e8feecc76d307d589550d13ecad
Ian Gopfert <iangoop@gmail.com>
2021-02-08 14:04:25 -0300

    Replacing data for generic ones to be used for other users

--- src/data.js
+++ src/data.js
@@ -1,21 +1,21 @@
 module.exports = {
-  "title": "Ian Gopfert | Software Engineer Resume",
-  "name": "Ian Gopfert",
-  "position": "Software Engineer",
+  "title": "Your name | Your role Resume",
+  "name": "Your name",
+  "position": "Your role",
   "info": {
-    "birthdate": "08/02/1986",
-    "location": "London, UK",
-    "email": "iangoop@gmail.com",
-    "mobile": "073 7775 4629",
+    "birthdate": "dd/mm/yyyy",
+    "location": "City, Country",
+    "email": "your@email.com",
+    "mobile": "077 7777 7777",
     "linkedin": {
-      "alias": "ian-gopfert",
-      "link": "http://www.linkedin.com/in/ian-gopfert"
+      "alias": "in-account",
+      "link": "http://www.linkedin.com/in/in-account"
     },
     "github": {
-      "alias": "iangoop",
-      "link": "http://www.github.com/iangoop"
+      "alias": "yourgitprofile",
+      "link": "http://www.github.com/yourgitprofile"
     },
-    "website": "iangoop.github.io"
+    "website": "yourgitprofile.github.io"
   },
   "languages": [
     {"name":"Portuguese", "level": "Native"},
@@ -81,9 +81,9 @@ module.exports = {
     }
   ],
   "profile": [
-      "A software engineer with 10+ years of experience in developing web projects on a set of platforms either as a freelance worker or as part of an enterprise team.",
-      "Experience in developing projects from scratch to deployment, as well as in maintaining and bulking up applications already in production. Applied different development life cycle models according to the nature of each project, from agile to more restrictive models.",
-      "Worked with Java and Oracle in enterprise development, using frameworks like Hibernate, Spring, and JSF. Migrated to the freelancing market, focusing on developing Responsive Single Page Application with AngularJS, using PHP and MySQL in the backend, providing RESTful APIs by using Slim Framework. Started using ExpressJS/NodeJS and MongoDB on personal projects."
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec nisl odio, efficitur in rutrum ut, venenatis tincidunt velit. Praesent neque erat, maximus eu justo eu, elementum eleifend erat.",
+      "Vivamus massa massa, bibendum in nunc eget, laoreet tempor purus. Quisque laoreet lorem mauris, mollis porttitor ipsum imperdiet a.",
+      "Cras in orci libero. Praesent ullamcorper interdum nibh vel ultrices. Duis pharetra nulla sed orci pharetra dictum."
   ],
   "experience": [
     {
@@ -91,40 +91,40 @@ module.exports = {
       "period":"2014 - 2020",
       "place":"Brazil",
       "highlights":[
-          "Worked on more than 13 web projects for Brazilian customers.",
+          "Phasellus sit amet vehicula lectus, ac elementum sapien. Nulla non risus vulputate, vestibulum leo ac, vulputate mauris.",
           {
-            "group":"Main projects:",
+            "group":"Donec vitae magna risus:",
             "highlights": [
-                "Deployed and maintained school administration portal in core PHP for 7 institutions, providing interfaces for students, teachers, and finance team, tailoring the software for each institution according to their needs.",
-                "Developed a multi-module web software for a digital marketing course in AngularJS and PHP/Slim Framework, with more than 15k users registered on the free open trial. Developed an upgraded PRO version due to the success of the first version.",
-                "Developed a platform using PHP/Slim Framework and AngularJS for environmental impacts awareness in local communities due to the deployment of new energy transmission lines, as an interactive web quiz, with a thousand interactive sessions of people from 5 different affected states.",
-                "Participated in the development of a platform for the application on the innovation awards of the Brazilian National Confederation of Industry, using Spring MVC/AngularJS, with almost 4k applications on its launch year."
+                "Maecenas convallis congue vulputate. Integer et malesuada lectus.",
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut malesuada sodales nulla a egestas.",
+                "Nunc sit amet augue euismod, vulputate lectus vel, auctor mi.",
+                "Curabitur dictum dui placerat, venenatis ipsum nec, fringilla felis."
             ]
           },
-          "Worked under several types of contracts following multiple development models, such as waterfall, incremental, and Kanban."
+          "Curabitur est eros, condimentum at metus ut, malesuada dignissim ligula."
       ]
     },
     {
-      "role": "System Analyst/Full Stack Developer, ADDLabs/UFF",
+      "role": "Full Stack Developer, Company",
       "period":"03/2010 - 04/2014",
       "place":"Rio de Janeiro, Brazil",
       "highlights":[
-        "Developed AI-based applications in Java to support an oil industry on intricate oil exploration processes.",
-        "Worked in every step of development, from the identification of the requirements, to back end and front end development as well as managing the deployment.",
+        "Curabitur et tempor velit, sed ultricies augue. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        "Maecenas erat lectus, congue ut feugiat vitae, laoreet ullamcorper ligula.",
         "Implemented complex interfaces by mixing JSF/Primefaces components with powerful jQuery front end coding",
-        "Led a team of 5+ person, including designer, researcher, interns, and other developers, being responsible for the interface between the team and product owner/manager"
+        "Sed consequat eu sem in ultrices. Donec vitae tortor non nunc interdum eleifend sed non turpis."
       ]
     }
   ],
   "projects": [
     {
-      "title": "AngularJS components",
-      "link": "http://npmjs.com/~ian.gopfert",
-      "alias": "~ian.gopfert",
+      "title": "Npm components",
+      "link": "http://npmjs.com/~yournpmprofile",
+      "alias": "~yournpmprofile",
       "highlights": [
-          "Developed a directive to apply input masks with variable currency configurations",
-          "Developed an HTTP interceptor for managing security tokens in a SPA Architecture, compatible with refresh tokens",
-          "Developed a flexible AngularJS wrapper for Popper.js, adding meaningful behavior to the library"
+          "Nullam tempor risus vitae augue mattis maximus. Cras tellus velit, eleifend quis suscipit vel, tristique a arcu.",
+          "Nullam sodales mollis est, aliquet lobortis eros volutpat et.",
+          "Maecenas porttitor, lacus at porttitor tempus, ligula dolor tempus ligula, at pellentesque justo velit nec ex. Praesent facilisis turpis tincidunt, maximus nisi eu, maximus ante."
       ]
     }
   ]
